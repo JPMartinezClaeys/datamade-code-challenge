6cbf90c66c753685c20b020904ed0d384190adc7
JPMartinez <jpmartinez@uchicago.edu>
2024-07-22 11:49:20 -0500

    First pass at views file

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -1,24 +1,59 @@
 import usaddress
+from typing import Tuple, Dict
 from django.views.generic import TemplateView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 from rest_framework.exceptions import ParseError
+import logging
+
+# Logging configuration
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 class Home(TemplateView):
-    template_name = 'parserator_web/index.html'
+    template_name = "parserator_web/index.html"
 
 
 class AddressParse(APIView):
     renderer_classes = [JSONRenderer]
 
-    def get(self, request):
+    def get(self, request) -> Response:
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        return Response({})
 
-    def parse(self, address):
+        # Obtain request from search
+        address = request.query_params.get("address", "")
+        if not address:
+            logging.info(f"Address not received")
+            return Response({"error": "No address provided"}, status=400)
+
+        # Parse input address
+        try:
+            address_componenets, address_type = self.parse(address)
+        except usaaddress.RepeatedLabelError as e:
+            logging.info(f"Parsing address {address} not succesful: {e}")
+            return Response({"error": f"{str(e)}"}, status=400)
+        except Excepection as e:
+            logging.info(f"Parsing address {address} not succesful: {e}")
+            return Response({"error": f"{str(e)}"}, status=400)
+
+        # Compile results
+        data = {
+            "input_string": address,
+            "address_components": address_components,
+            "address_type": address_type,
+        }
+
+        return Response(data)
+
+    def parse(self, address: str) -> Tuple[Dict, str]:
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
-        return address_components, address_type
+        try:
+            address_components, address_type = usaddress.tag(address)
+            return address_components, address_type
+        except Exception as e:
+            logging.info(f"Parsing address {address} not succesful: {e}")
--- tests/test_views.py
+++ tests/test_views.py
@@ -4,12 +4,12 @@ import pytest
 def test_api_parse_succeeds(client):
     # TODO: Finish this test. Send a request to the API and confirm that the
     # data comes back in the appropriate format.
-    address_string = '123 main st chicago il'
+    address_string = "123 main st chicago il"
     pytest.fail()
 
 
 def test_api_parse_raises_error(client):
     # TODO: Finish this test. The address_string below will raise a
     # RepeatedLabelError, so ParseAddress.parse() will not be able to parse it.
-    address_string = '123 main st chicago il 123 main st'
+    address_string = "123 main st chicago il 123 main st"
     pytest.fail()
